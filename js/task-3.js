8d824286d9c91c4b60000c037979f1ac58b0380a
Veronika Glinka <glinkaveronika@gmail.com>
2025-03-18 14:38:59 +0100

    hw 4 upload 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,24 @@
-function getElementWidth(content, padding, border) {
-  const numContent = Number.parseFloat(content);
-  const numPadding = Number.parseFloat(padding);
-  const numBorder = Number.parseFloat(border);
-  return numContent + numPadding * 2 + numBorder * 2;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
